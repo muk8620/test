4c04cb908a9848b6de8b3d8839d7444c64714692
devCJH <http-@naver.com>
2024-05-03 16:56:47 +0900

    article list

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,17 @@ public class Main {
 			}
 			
 			if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("존재하는 게시글이 없습니다");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				
+				for (int i = articles.size() - 1; i  >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
 				
 			} else if (cmd.equals("article write")) {
 				System.out.printf("제목 : ");
